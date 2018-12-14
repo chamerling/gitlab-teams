479874683269e0dafe26a1d4148d814b14950d64
Christophe HAMERLING <chamerling@linagora.com>
2018-12-14 17:28:39 +0100

    Validate fields

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="pa-3">
-    <v-form>
+    <v-form ref="form" v-model="valid">
       <v-text-field
         v-model="apiToken"
         label="API Token"
+        :rules="tokenRules"
         required
       ></v-text-field>
       <v-text-field
         v-model="apiEndpoint"
         label="Gitlab URL"
+        :rules="urlRules"
         required
       ></v-text-field>
-      <v-btn @click="submit">Submit</v-btn>
+      <v-btn
+        :disabled="!valid"
+        @click="submit">
+        Submit
+      </v-btn>
     </v-form>
   </div>
 </template>
@@ -21,6 +27,14 @@ export default {
   name: "settings",
   data() {
     return {
+      valid: false,
+      tokenRules: [
+        v => !!v || 'Token is required'
+      ],
+      urlRules: [
+        v => !!v || 'URL is required',
+        v => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(v) || 'URL is not valid'
+      ],
       apiToken: null,
       apiEndpoint: null
     };
@@ -31,11 +45,13 @@ export default {
   },
   methods: {
     submit() {
-      this.$store.dispatch("updateSettings", {
-        apiToken: this.apiToken,
-        apiEndpoint: this.apiEndpoint
-      });
-      this.$router.push("/");
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch("updateSettings", {
+          apiToken: this.apiToken,
+          apiEndpoint: this.apiEndpoint
+        });
+        this.$router.push("/");
+      }
     }
   },
   components: {}
