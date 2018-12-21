eb1ef7183b3d982cb9ce598cf42d281c77542f2c
Christophe HAMERLING <chamerling@linagora.com>
2018-12-21 14:52:11 +0100

    Be able to define BASE_URL from env

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === "production" ? "/gitlab-teams/" : "/"
+  baseUrl: process.env.NODE_ENV === "production" ? process.env.BASE_URL || "/gitlab-teams/" : "/",
 };
