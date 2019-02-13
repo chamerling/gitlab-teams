366e946784dd2c5d5702128d34210afc1deff409
Christophe HAMERLING <chamerling@linagora.com>
2019-02-13 18:33:09 +0100

    Display running pipelines

--- src/components/AvatarBuildGauge.vue
+++ src/components/AvatarBuildGauge.vue
@@ -4,7 +4,7 @@
       <v-badge overlap>
         <span slot="badge">{{ mergeRequests.length }}</span>
         <user-avatar-popover :user="user">
-          <span v-if="stats.success">Pipelines: {{stats.success}} success, {{stats.failed}} failed</span>
+          <span v-if="stats.success || stats.failed || starts.running">Pipelines: {{stats.success}} success, {{stats.failed}} failed, {{stats.running}} running</span>
         </user-avatar-popover>
       </v-badge>
     </v-progress-circular>
@@ -30,6 +30,7 @@ export default {
       const pipelines = this.mergeRequests.map(mergeRequest => this.getPipeline(mergeRequest)).filter(Boolean);
       const success = pipelines.filter(pipeline => pipeline.status === "success");
       const failed = pipelines.filter(pipeline => pipeline.status === "failed");
+      const running = pipelines.filter(pipeline => pipeline.status === "running");
       const isRunning = pipelines.some(pipeline => pipeline.status === "running");
       const ratio = ((success.length * 100) / pipelines.length).toFixed(2);
       const color = ['error', 'warning', 'success'][Math.floor(ratio / 40)] || "grey darken-3";
@@ -39,6 +40,7 @@ export default {
         color,
         success: success.length,
         failed: failed.length,
+        running: running.length,
         pipelines: pipelines.length,
         isRunning
       };
