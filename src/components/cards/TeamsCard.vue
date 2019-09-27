24c10db05740f2bf3dc29976563766cc94597374
sibinthomas007 <sibin.thomas@practo.com>
2019-09-27 15:04:38 +0530

    adding space

--- src/components/cards/TeamsCard.vue
+++ src/components/cards/TeamsCard.vue
@@ -6,7 +6,7 @@
     </v-card-title>
     <v-card-text>
       <div class="teams ma-1">
-        <div @click="goTo(team)"class="team ma-1" v-for="team in teams" :key="team.name">
+        <div @click="goTo(team)" class="team ma-1" v-for="team in teams" :key="team.name">
           <v-tooltip bottom>
             <team-avatar slot="activator" :team="team" :size="60"/>
             <span>{{team.name}}</span>
