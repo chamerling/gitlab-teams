19f8c0067ca79e05d20bb24f5f08d128fd3960af
Martijn <Martijn.van.Adrichem@gmail.com>
2019-10-15 21:14:51 +0200

    Adds button to teams card when no teams are found

---
+++ src/components/CreateTeamDialog.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-dialog v-model="showDialog" width="500">
+      <v-card>
+        <v-toolbar dark>
+          <v-btn icon dark @click="showDialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title class="white--text">Create a new team</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <CreateTeam @close="showDialog = false" />
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+import CreateTeam from "@/components/CreateTeam.vue";
+
+export default {
+  props: {
+    show: Boolean
+  },
+  computed: {
+    showDialog: {
+      get() {
+        return this.show;
+      },
+      set(show) {
+        this.$emit("close", show);
+      }
+    }
+  },
+  components: {
+    CreateTeam
+  }
+};
+</script>
--- src/components/cards/TeamsCard.vue
+++ src/components/cards/TeamsCard.vue
@@ -6,12 +6,16 @@
     </v-card-title>
     <v-card-text>
       <div class="teams ma-1">
+        <v-btn fab ripple @click="dialog = true" v-if="teams.length === 0">
+          <v-icon color="grey">add_circle_outline</v-icon>
+        </v-btn>
         <div @click="goTo(team)" class="team ma-1" v-for="team in teams" :key="team.name">
           <v-tooltip bottom>
             <avatar slot="activator" :name="team.name" :size="60"/>
             <span>{{team.name}}</span>
           </v-tooltip>
         </div>
+        <create-team-dialog :show="dialog" @close="changeDialog"></create-team-dialog>
       </div>
     </v-card-text>
   </v-card>
@@ -19,18 +23,26 @@
 
 <script>
 import Avatar from "@/components/Avatar.vue";
+import CreateTeamDialog from "@/components/CreateTeamDialog.vue";
 
 export default {
   props: {
     teams: Array
   },
+  data: () => ({
+    dialog: false
+  }),
   methods: {
     goTo(team) {
       this.$router.push({ name: "team", params: { name: team.name } });
+    },
+    changeDialog(dialog) {
+      this.dialog = dialog;
     }
   },
   components: {
-    Avatar
+    Avatar,
+    CreateTeamDialog
   }
 };
 </script>
