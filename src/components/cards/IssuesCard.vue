76818a1081807750d40b20c34e09ae0df68e9f71
patcodefy <irapatu44@gmail.com>
2019-10-24 00:45:20 -0700

    Updated Issues widget to display the last 5 issues.

--- src/components/cards/IssuesCard.vue
+++ src/components/cards/IssuesCard.vue
@@ -3,19 +3,17 @@
     <div id="count" class="ma-1">
       <span class="font-weight-medium display-3">{{total}}</span>
     </div>
-    <v-tooltip bottom>
+    <v-tooltip bottom v-for="(issue,index) in this.getIssues()" :key="index">
       <v-list slot="activator" v-if="issues.length" two-line dense>
-        <issue :item="last" hide-actions/>
+        <issue :item="issue" hide-actions/>
       </v-list>
-      <span v-if="last">Last issue: {{last.title}}</span>
+      <span v-if="issue.title">Last issue: {{issue.title}}</span>
     </v-tooltip>
   </div>
 </template>
 
 <script>
-import { head } from "lodash";
 import Issue from "@/components/Issue.vue";
-
 export default {
   props: {
     issues: Array,
@@ -24,13 +22,14 @@ export default {
       default: 0
     }
   },
-  computed: {
-    last() {
-      return head(this.issues);
-    }
-  },
   components: {
     Issue
+  },
+  methods: {
+    getIssues: function() {
+      let lastIssues = _.slice(this.issues, [this.issues.length - 5], [this.issues.length]);
+      return this.issues.length < 5 ? this.issues : lastIssues;
+    }
   }
 };
 </script>
