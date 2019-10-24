80dd4eaead4ae733600aaf22456dad65f5cd8728
patcodefy <irapatu44@gmail.com>
2019-10-24 09:16:47 -0700

    corrected the code based on the code review

--- src/components/cards/IssuesCard.vue
+++ src/components/cards/IssuesCard.vue
@@ -3,15 +3,16 @@
     <div id="count" class="ma-1">
       <span class="font-weight-medium display-3">{{total}}</span>
     </div>
-    <v-tooltip bottom v-for="(issue,index) in this.getIssues()" :key="index">
+    <v-tooltip bottom v-for="issue in last" :key="issue.id">
       <v-list slot="activator" v-if="issues.length" two-line dense>
         <issue :item="issue" hide-actions/>
       </v-list>
-      <span v-if="issue.title">Last issue: {{issue.title}}</span>
+      <span v-if="issue">{{issue.title}}</span>
     </v-tooltip>
   </div>
 </template>
 <script>
+import { slice } from "lodash";
 import Issue from "@/components/Issue.vue";
 export default {
   props: {
@@ -21,14 +22,13 @@ export default {
       default: 0
     }
   },
+  computed: {
+    last() {
+      return slice(this.issues, 0, 5);
+    }
+  },
   components: {
     Issue
-  },
-  methods: {
-    getIssues: function() {
-      let lastIssues = _.slice(this.issues, [this.issues.length - 5], [this.issues.length]);
-      return this.issues.length < 5 ? this.issues : lastIssues;
-    }
   }
 };
 </script>
